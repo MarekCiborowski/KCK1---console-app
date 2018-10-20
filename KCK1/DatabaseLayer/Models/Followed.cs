e7e78f493254ae03f2c8852a70bc78066271ebf4
Marek Ciborowski <senthi236@gmail.com>
2018-10-20 12:37:21 +0200

    Sequence contains no matching element, zamknij ryj

---
+++ KCK1/DatabaseLayer/Models/Followed.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLayer.Models
+{
+    public class Followed
+    {
+        [Key, ForeignKey("Follower")]
+        public int FollowerUserId { get; set; }
+        [Key, ForeignKey("Following")]
+        public int FollowUserId { get; set; }
+
+        
+
+        [InverseProperty("FollowedUsers")]  
+        public virtual Account Follower { get; set; }
+        [InverseProperty("FollowingUsers")]  
+        public virtual Account Following { get; set; }
+    }
+}
