37badee90b8ea262e94bf752f0d4efe50c36f51f
Michał Kuc <38427529+mkkuc@users.noreply.github.com>
2018-10-31 17:41:50 +0100

    art asci elo

---
+++ KCK1/Surveys/Views/SignIn.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = Colorful.Console;
+using System.Drawing;
+using DatabaseLayer;
+using DatabaseLayer.Models;
+using RepositoryLayer.Repositories;
+
+namespace Surveys.Views
+{
+    public class SignIn
+    {
+        public static void Start()
+        {
+            setConsoleSize();
+
+
+        }
+
+        public static void setConsoleSize()
+        {
+            int heightOfWindow = 30;
+            int widthOfWindow = 100;
+            Console.SetWindowSize(widthOfWindow, heightOfWindow);
+            Console.Clear();
+        }
+
+        public static string getMainTitleString()
+        {
+            string survey = @"
+                    _________    ___    ____    ________    ___       __   _________    _________  
+                   |\    ____\  |\  \  |\   \  |\   __  \  |\  \     |  | |\   _____\  |\______  \
+                   \ \   \___|_ \ \  \  \ \  \ \ \  \|\  \ \ \  \    |  | \ \  \        \|_____\  \
+                    \ \_____   \ \ \  \  \ \  \ \ \       \ \ \  \   |  |  \ \  \___            \  \
+                     \|_____|\  \ \ \  \__\_\  \ \ \   _  _\ \ \  \  |  |   \ \   __\       __   \  \
+                       _____|_\  \ \ \          \ \ \  \\  \  \ \  \ |  |    \ \  \______  |\ \___\  \
+                      |\__________\ \_\__________\ \ \__\\ _\  \ \  \|  |     \ \________\ \ \________\
+                      \|__________|  \|__________|  \|__|\|__|  \_\_____|      \|________|  \|________|";
+            return survey;
+        }
+        
+    }
+}
