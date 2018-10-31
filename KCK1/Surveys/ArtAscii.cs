9034361731e4f3e7005b083b985ffa4ec6c2c91d
Michał Kuc <38427529+mkkuc@users.noreply.github.com>
2018-10-31 18:53:29 +0100

    o to

---
+++ KCK1/Surveys/ArtAscii.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surveys
+{
+    public class ArtAscii
+    {
+        public static string getMainTitleString()
+        {
+            string survey = @"
+                 _________    ___    ____    ________    ___       __   _________    _________  
+                |\    ____\  |\  \  |\   \  |\   __  \  |\  \     |  | |\   _____\  |\______  \
+                \ \   \___|_ \ \  \  \ \  \ \ \  \|\  \ \ \  \    |  | \ \  \        \|_____\  \
+                 \ \_____   \ \ \  \  \ \  \ \ \       \ \ \  \   |  |  \ \  \___            \  \
+                  \|_____|\  \ \ \  \__\_\  \ \ \   _  _\ \ \  \  |  |   \ \   __\       __   \  \
+                    _____|_\  \ \ \          \ \ \  \\  \  \ \  \ |  |    \ \  \______  |\ \___\  \
+                   |\__________\ \_\__________\ \ \__\\ _\  \ \  \|  |     \ \________\ \ \________\
+                   \|__________|  \|__________|  \|__|\|__|  \_\_____|      \|________|  \|________|";
+            return survey;
+        }
+    }
+}
---
+++ KCK1/Surveys/Views/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using DatabaseLayer;
+using DatabaseLayer.Models;
+using RepositoryLayer.Repositories;
+
+namespace Surveys.Views
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Configuration.ConsoleInitialize();
+            Start();
+
+        }
+        public static void Start()
+        {
+            Configuration.setConsoleSize();
+
+            Console.WriteLine(ArtAscii.getMainTitleString());
+            Console.SetCursorPosition(Console.WindowWidth / 3, 10);
+            Console.WriteLine("Press any button to continue");
+
+            Console.ReadKey();
+
+            SignInView.Menu();
+        }
+    }
+}
