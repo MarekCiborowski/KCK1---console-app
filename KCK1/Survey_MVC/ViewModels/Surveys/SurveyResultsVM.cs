e55e715aaf725df8f14a5f7f46175d1dd4ff2912
Marek Ciborowski <senthi236@gmail.com>
2018-11-23 13:18:03 +0100

    modelki

---
+++ KCK1/Survey_MVC/ViewModels/Surveys/SurveyResultsVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Survey_MVC.ViewModels.Surveys
+{
+    public class SurveyResultsVM
+    {
+    }
+}
